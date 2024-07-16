5b35fd45ef4f947e43dec738760c8785443c9cb7
palontologist <100496915+palontologist@users.noreply.github.com>
2024-07-16 13:36:09 +0300

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,33 @@
+import createNextDocsMDX from "fumadocs-mdx/config";
+
+const withMDX = createNextDocsMDX();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "**",
+      }
+    ],
+  },
+};
+
+export default withMDX(nextConfig);
